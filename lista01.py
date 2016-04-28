e8bc17cf6ddfeb8d0a8557fb2d62948f684b18f1
Adriele <adrgarcez@users.noreply.github.com>
2016-04-28 09:57:19 -0300

    Update lista01.py

--- lista01.py
+++ lista01.py
@@ -1,4 +1,4 @@
-#Faça um Programa que leia uma lista de 5 números inteiros e mostre-os. 
+# Faça um Programa que leia uma lista de 5 números inteiros e mostre-os. 
 
 #MANEIRA 1
 print('QUESTAO 01')
@@ -12,8 +12,8 @@ for inicio in range (5):
 print ('LISTA =',lista)
 
 '''------------------------------------------------------------------------------------------------'''
+# Faça um Programa que leia uma lista de 10 números reais e mostre-os na ordem inversa.
 
-#2
 #MANEIRA 1
 print('QUESTAO 02')
 lista = [1, 2, 5, 78, 9, 48, 75, 26, 3, 100]
@@ -21,18 +21,17 @@ lista.reverse()
 print ('ORDEM INVERSA =',lista)
 
 #MANEIRA 2
-'''
 lista = []
 for inicio in range (10):
     lista.append(int(input('Digite um numero:')))
 lista.reverse()
 print ('LISTA =',lista)
-'''
+
 print('\n')
 
 '''------------------------------------------------------------------------------------------------'''
+# Faça um Programa que leia 4 notas, mostre as notas e a média na tela. 
 
-#3
 print('QUESTAO 03')
 #MANEIRA 1
 lista = [10, 9.5, 9, 8]
@@ -44,7 +43,6 @@ media = soma / len(lista)
 print ('MEDIA =',media)
 
 #MANEIRA 2
-'''
 lista = []
 media = soma = 0
 for inicio in range(4):
@@ -55,11 +53,12 @@ for inicio in lista:
 media = soma / len(lista)
 print ('SOMA =',soma)
 print ('MEDIA =',media)
-'''
+
 print('\n')
 
+'''------------------------------------------------------------------------------------------------'''
+# Faça um Programa que leia uma lista de 10 caracteres, e diga quantas consoantes foram lidas. Imprima as consoantes. 
 
-#4
 #MANEIRA 1
 print('QUESTAO 04')
 lista = ['w','t','u','m','a','y','p','o','b','e']
@@ -72,7 +71,6 @@ for inicio in lista:
 print ('TOTAL DE CONSOANTES =',contador)
 
 #MANEIRA 2
-'''
 lista = []
 contador = 0
 for inicio in range(10):
@@ -82,10 +80,13 @@ for inicio in lista:
         print (inicio)
         contador = contador + 1
 print ('TOTAL DE CONSOANTES =',contador)
-'''
+
 print('\n')
 
-#5
+'''------------------------------------------------------------------------------------------------'''
+# Faça um Programa que leia 20 números inteiros e armazene-os numa lista. 
+# Armazene os números pares na listar PAR e os números IMPARES na lista impar. Imprima os três vetores. 
+
 #MANEIRA 1
 print('QUESTAO 05')
 numeros = [2,4,5,8,96,35,78,41,25,698,52,17,100,256,968,632,547,852,301,605]
@@ -99,7 +100,6 @@ for inicio in numeros:
 print('LISTA =',numeros,'\nPAR =',par,'\nIMPAR =',impar)
 
 #MANEIRA 2
-'''
 numeros = []
 impar = []
 par = []
@@ -111,11 +111,13 @@ for inicio in numeros:
     else:
         impar.append(inicio)
 print('LISTA =',numeros,'\nPAR =',par,'\nIMPAR =',impar)
-'''
+
 print('\n')
 
-#6
-'''
+'''------------------------------------------------------------------------------------------------'''
+# Faça um Programa que peça as quatro notas de 10 alunos, calcule e armazene numa lista a média de cada aluno, 
+# imprima o número de alunos com média maior ou igual a 7.0. 
+
 print('QUESTAO 06')
 lista = []
 nota = media = count = 0
@@ -134,8 +136,11 @@ for inicio in lista:
 print('QUANTIDADE DE ALUNOS COM MEDIA IGUAL OU MAIOR QUE 7 =',count)
 
 print('\n')
-'''
-#7
+
+'''------------------------------------------------------------------------------------------------'''
+# Faça um Programa que leia duas listas com 10 elementos cada. 
+# Gere uma terceira lista de 20 elementos, cujos valores deverão ser compostos pelos elementos intercalados das duas outras listas. 
+
 print('QUESTAO 07')
 lista1 = [1, 20, 10, 48, 7, 59, 638, 85, 72, 93]
 lista2 = [24, 69, 874, 32, 64, 51, 28, 60, 37, 2]
@@ -149,7 +154,9 @@ print ("LISTA INTERCALADA:",lista3)
 
 print('\n')
 
-#8
+'''------------------------------------------------------------------------------------------------'''
+# Faça um programa que receba a temperatura média de cada mês do ano e armazene-as em uma lista. 
+# Após isto, calcule a média anual das temperaturas e mostre todas as temperaturas acima da média anual, e em que mês elas ocorreram (mostrar o mês por extenso: 1 – Janeiro, 2 – Fevereiro, . . . ). 
 
 print('QUESTAO 08')
 temperaturas = []
@@ -169,7 +176,19 @@ for inicio in range(len(temperaturas)):
 
 print('\n')
 
-#9
+'''------------------------------------------------------------------------------------------------'''
+'''
+Utilizando listas faça um programa que faça 5 perguntas para uma pessoa sobre um crime. As perguntas são: 
+a) "Telefonou para a vítima?" 
+b) "Esteve no local do crime?" 
+c) "Mora perto da vítima?" 
+d) "Devia para a vítima?" 
+e) "Já trabalhou com a vítima?" 
+O programa deve no final emitir uma classificação sobre a participação da pessoa no crime. 
+Se a pessoa responder positivamente a 2 questões ela deve ser classificada como "Suspeita", entre 3 e 4 como "Cúmplice" e 5 como "Assassino". 
+Caso contrário, ele será classificado como "Inocente". 
+'''
+
 print('QUESTAO 09')
 perguntas=["Telefonou para a vitima?","Esteve no local do crime?","Mora perto da vitima?","Devia para a vitima?","Ja trabalhou com a vitima?"]
 contador = 0
@@ -189,8 +208,20 @@ elif (contador<2):
 
 print('\n')
 
+'''------------------------------------------------------------------------------------------------'''
+'''
+Faça um programa que leia um número indeterminado de valores, correspondentes a notas, encerrando a entrada de dados quando for informado um valor igual a -1 (que não deve ser armazenado). 
+Após esta entrada de dados, faça: 
+a) Mostre a quantidade de valores que foram lidos; 
+b) Exiba todos os valores na ordem em que foram informados, um ao lado do outro; 
+c) Exiba todos os valores na ordem inversa à que foram informados, um abaixo do outro; 
+d) Calcule e mostre a soma dos valores; 
+e) Calcule e mostre a média dos valores; 
+f) Calcule e mostre a quantidade de valores acima da média calculada; 
+g) Calcule e mostre a quantidade de valores abaixo de sete; 
+h) Encerre o programa com uma mensagem;
+'''
 
-#10
 print('QUESTAO 10')
 notas=[]
 nota=soma=contador=0
